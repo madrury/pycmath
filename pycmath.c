aff932edb978158a92f43aef8138234175094126
madrury <matthew.drury.83@gmail.com>
2016-06-06 14:11:32 -0700

    Stub implementation of primes(n)

--- pycmath.c
+++ pycmath.c
@@ -1,31 +1,47 @@
 #include <Python.h>
 
+/* A simple vector datatype.
+   Essentially a bounded array, with the length information attached.
+*/
+struct vector {
+    long length;
+    long* data;
+};
 
-void make_primes_array(long n, long* primes_array, long* n_primes) {
-    *n_primes = 2;
-    primes_array = malloc(sizeof(long) * *n_primes);
-    primes_array[0] = 1;
-    primes_array[0] = 2;
+struct vector* make_primes_vector(long n) {
+    struct vector* v = malloc(sizeof(struct vector));
+    v->length = n;
+    v->data = malloc(sizeof(long) * n);
+    for(long i = 0; i < n; i++) {
+        v->data[i] = i;
+    }
+    return v;
 }
+
+void free_vector(struct vector* v) {
+    free(v->data);
+    free(v);
+}
+
+
+/* Python methods for module. */
 
 static PyObject* pycmath_primes(PyObject* self, PyObject *args) {
     const long n;              // Argument: we are calculating the primes <= n
-    //long* primes_array = NULL; // A C array to hold the computed primes.
-    //long n_primes;             // How many primes did we find?
     PyObject* primes;          // A python list to hold the primes we found.
 
     if (!PyArg_ParseTuple(args, "l", &n))
         return NULL;
     
-    //make_primes_array(n, primes_array, &n_primes);
+    struct vector* primes_vector = make_primes_vector(n);
 
     primes = PyList_New(0);
     Py_INCREF(primes);
-    for(long i = 0; i < n; i++) {
-        PyList_Append(primes, PyLong_FromLong(i));
+    for(long i = 0; i < primes_vector->length; i++) {
+        PyList_Append(primes, PyLong_FromLong(primes_vector->data[i]));
     }
 
-    //free(primes_array);
+    free_vector(primes_vector);
     return primes;
 }
 
